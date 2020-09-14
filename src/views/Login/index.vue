4dbd2631499e2dad78daaafc3b5236aca4d0b89f
ldb <grindel_wald@163.com>
2020-09-14 17:56:30 +0800

    登录 tab 点击事件

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,26 +1,10 @@
 <template>
     <div id="login">
-        <div id="box">
-            <div class="form">
-                <div>
-                    <div class="flex_lr">
-                        <p class="font w_70">账户:</p>
-                        <el-input placeholder="请输入账户" v-model="name" clearable></el-input>
-                    </div>
-                    <div class="flex_lr" style="margin-top:20px">
-                        <p class="font w_70">密码:</p>
-                        <el-input placeholder="请输入密码" v-model="password" show-password></el-input>
-                    </div>
-                </div>
-                <div class="login_btn flex_left">
-                    <div>
-                        <el-button type="primary" round>Login</el-button>
-                    </div>
-                    <div>
-                        <el-button type="info" round>Reset</el-button>
-                    </div>
-                </div>
-            </div>
+        <div class="login-wrap">
+            <ul class="menu-tab">
+                <li :class="{'current':tabName=='login'}" @click="tabClick('login')">Login</li>
+                <li :class="{'current':tabName=='reset'}" @click="tabClick('reset')">Reset</li>
+            </ul>
         </div>
     </div>
 </template>
@@ -30,10 +14,15 @@ export default {
     data() {
         return {
             name: "",
-            password: ""
+            password: "",
+            tabName: "login"
         };
     },
-    methods: {}
+    methods: {
+        tabClick(txt) {
+            this.tabName = txt;
+        }
+    }
 };
 </script>
 
@@ -41,18 +30,24 @@ export default {
 #login {
     height: 100vh;
     background-color: #344a5f;
-    #box {
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .form {
-            width: 300px;
-            .login_btn {
-                margin-top: 30px;
-                padding: 20px;
-            }
-        }
+}
+.login-wrap {
+    width: 330px;
+    margin: 0 auto;
+}
+.menu-tab {
+    text-align: center;
+    li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: #fff;
+        border-radius: 2px;
+        cursor: pointer;
+    }
+    .current {
+        background-color: rgba(0, 0, 0, 0.1);
     }
 }
 </style>
