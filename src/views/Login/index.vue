13ff976ccfa45877f171bc7e8afb6e17d3391701
ldb <grindel_wald@163.com>
2020-09-15 17:42:47 +0800

    登录页面样式完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,23 +5,134 @@
                 <li :class="{'current':tabName=='login'}" @click="tabClick('login')">Login</li>
                 <li :class="{'current':tabName=='reset'}" @click="tabClick('reset')">Reset</li>
             </ul>
+            <el-form
+                :model="ruleForm"
+                :rules="rules"
+                ref="ruleForm"
+                size="medium"
+                class="login-form"
+            >
+                <el-form-item prop="email" class="item-form">
+                    <label>邮箱</label>
+                    <el-input type="txt" v-model="ruleForm.email" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item prop="pass" class="item-form">
+                    <label>密码</label>
+                    <el-input
+                        v-model="ruleForm.pass"
+                        autocomplete="off"
+                        minlength="6"
+                        maxlength="25"
+                        show-password
+                    ></el-input>
+                </el-form-item>
+                <el-form-item prop="password" class="item-form" v-if="tabName=='reset'">
+                    <label>再次输入密码</label>
+                    <el-input
+                        show-password
+                        v-model="ruleForm.password"
+                        autocomplete="off"
+                        minlength="6"
+                        maxlength="25"
+                    ></el-input>
+                </el-form-item>
+                <el-form-item prop="code" class="item-form">
+                    <label>验证码</label>
+                    <el-row :gutter="10">
+                        <el-col :span="15">
+                            <el-input v-model.number="ruleForm.code"></el-input>
+                        </el-col>
+                        <el-col :span="9">
+                            <el-button type="success" class="block">获取验证码</el-button>
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+                <el-form-item class="btn-login">
+                    <el-button type="danger" @click="submitForm('ruleForm')" class="block">提交</el-button>
+                </el-form-item>
+            </el-form>
         </div>
     </div>
 </template>
 
-<script>
+<script lang=js>
 export default {
     data() {
+        
+        
+
+         var validateEmail = (rule, value, callback) => {
+            let emailReg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            if (value === "") {
+                callback(new Error("请输入邮箱"));
+            } else if(!emailReg.test(value)){
+                
+                callback(new Error("邮箱地址输入有误"));
+            } else{
+                callback()
+            }
+        };
+        var validatePass = (rule, value, callback) => {
+            if (value === "") {
+                callback(new Error("请输入密码"));
+            } else {
+                
+                callback();
+            }
+        };
+        var validatePassword = (rule, value, callback) => {
+            console.log(this.ruleForm.pass)
+            if (value === "") {
+                callback(new Error("请输入密码"));
+            } else if(this.ruleForm.pass != value){
+                
+                callback(new Error("两次输入密码不一致"));
+            } else {
+                callback()
+            }
+        };
+        var validateCode = (rule, value, callback) => {
+            if (value === "") {
+                callback(new Error("请输入验证码"));
+            } else {
+                
+                callback();
+            }
+        };
+       
+
         return {
-            name: "",
-            password: "",
-            tabName: "login"
+            tabName: "login",
+            ruleForm: {
+                email: "",
+                pass: "",
+                password:"",
+                code: ""
+            },
+            rules: {
+                email: [{ validator: validateEmail, trigger: "blur" }],
+                pass: [{ validator: validatePass, trigger: "blur" }],
+                password: [{ validator: validatePassword, trigger: "blur" }],
+                code: [{ validator: validateCode, trigger: "blur" }]
+            }
         };
     },
     methods: {
         tabClick(txt) {
             this.tabName = txt;
-        }
+            this.$refs["ruleForm"].resetFields(); //重置表单
+        },
+        submitForm(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    alert("submit!");
+                } else {
+                    console.log("error submit!!");
+                    return false;
+                }
+            });
+        },
+       
     }
 };
 </script>
@@ -49,5 +160,24 @@ export default {
     .current {
         background-color: rgba(0, 0, 0, 0.1);
     }
+}
+.login-form {
+    margin-top: 30px;
+    label {
+        display: block;
+        font-size: 14px;
+        color: #fff;
+        margin-bottom: 3px;
+    }
+    .item-form {
+        margin-bottom: 3px;
+    }
+    .btn-login {
+        margin-top: 20px;
+    }
+}
+.block {
+    display: block;
+    width: 100%;
 }
 </style>
